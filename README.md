7d7512a79b18251e713771f1a8d24b788cc8f2ed
flo <you@example.com>
2019-02-11 15:55:20 +0100

    chore: remove port 4848
        payara micro doesnt come with the payara
        admin console

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ mvn clean package && docker build -t com.example/config-example .
 
 # RUN
 
-docker rm -f config-example || true && docker run -d -p 8080:8080 -p 4848:4848 --name config-example com.example/config-example 
+docker rm -f config-example || true && docker run -d -p 8080:8080 --name config-example com.example/config-example
--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn clean package && docker build -t com.example/config-example .
-docker rm -f config-example || true && docker run -d -p 8080:8080 -p 4848:4848 --name config-example com.example/config-example 
+docker rm -f config-example || true && docker run -d -p 8080:8080 --name config-example com.example/config-example
